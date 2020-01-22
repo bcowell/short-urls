21d81595f2d0dde492ac0d6e086a4a961ae65fea
Brayden Cowell <bcowell@thekennagroup.com>
2020-01-22 15:05:09 -0500

    Barebones static UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header">
+        <h1>short-urls</h1>
       </header>
+      <div className="form-container">
+        <div className="row">
+          <input className="input"></input>
+          <button className="button">SHORTEN</button>
+        </div>
+        <div className="row">
+          <div className="short-url">http://localhost:3001/api/v1/y7</div>
+          <button className="button">COPY</button>
+        </div>
+      </div>
     </div>
   );
 }
